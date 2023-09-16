d43dcd8d313da0c0ad4977211c8cce2e7539f7b9
Rafael Ribeiro <rafaelribdev@gmail.com>
2023-09-15 22:17:37 -0300

    15/09/2023

---
+++ aula100.py
@@ -0,0 +1,13 @@
+import copy
+
+from dados import produtos
+
+novos_produtos = [
+    {**p, 'preco': round(p['preco'] * 1.1, 2)} 
+    for p in copy.deepcopy(produtos)
+]
+
+print(*produtos, sep='\n')
+print()
+print(*novos_produtos, sep='\n')
+print(50 * '-')
---
+++ dados/__init__.py
@@ -0,0 +1,2 @@
+# importando a variavel "produtos" do módulo "produtos_modulo" que está dentro do package "dados"
+from dados.produtos_modulo import produtos
